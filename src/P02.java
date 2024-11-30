f26815a7151beaf86109f281c1fac05ee0c1ef43
Seyyid <seyyidefekaraman@gmail.com>
2024-11-30 19:24:31 +0300

    first commit

---
+++ src/P02.java
@@ -0,0 +1,6 @@
+public class P02 {
+    public static void main(String[] args) {
+        System.out.println("Seyyid Efe KAraman");
+
+    }
+}
---
+++ src/P03.java
@@ -0,0 +1,5 @@
+public class P03 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
